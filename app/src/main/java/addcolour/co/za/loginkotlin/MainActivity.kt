9ec952bb99ff43b7a6a34e38d50d74810ab5a4ad
mcnkuna <mpho@cobiinteractive.com>
2018-07-19 14:33:14 +0200

    Data binding

--- app/src/main/java/addcolour/co/za/loginkotlin/MainActivity.kt
+++ app/src/main/java/addcolour/co/za/loginkotlin/MainActivity.kt
@@ -1,12 +1,22 @@
 package addcolour.co.za.loginkotlin
 
+import addcolour.co.za.loginkotlin.databinding.ActivityMainBinding
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private var mBinding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initialize()
+    }
+
+    private fun initialize() {
+        mBinding?.contentToolBar?.title = getString(R.string.git_repo_title)
     }
 }
